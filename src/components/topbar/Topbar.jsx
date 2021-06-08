45262c27ca1ef4b40165a1550b5a32e2d00c8d2e
JB <jbevangelista07@gmail.com>
2021-06-08 16:57:19 +0800

    Update Portfolio

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">My Portfolio</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+639565290841</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>jbevangelista07@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
